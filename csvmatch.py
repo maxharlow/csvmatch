d928c9010cfa98c1aacd0cba23b242e768f1217e
Max Harlow <maxharlow@gmail.com>
2015-12-13 11:56:47 +0000

    Various minor fixes

--- csvmatch.py
+++ csvmatch.py
@@ -13,16 +13,16 @@ def main():
         matches = match(data1, data2)
         results = output(data1, data2, fields1, fields2, matches)
         print(results)
-    except Exception as e: sys.exit(e)
+    except BaseException as e: sys.exit(e)
 
 def arguments():
     parser = argparse.ArgumentParser(description='find matches between two CSV files')
     parser.add_argument('FILE1', nargs='?', default='-', help='the first CSV file: results will be returned where this file matches the second file -- if omitted, will accept input on STDIN')
-    parser.add_argument('FILE2', nargs='?', help='the second CSV file: results will be returned where the first file matches this one')
+    parser.add_argument('FILE2', nargs='?', default='-', help='the second CSV file: results will be returned where the first file matches this one')
     parser.add_argument('-1', '--fields1', nargs='+', type=str, help='one or more column names from the first file that should be used (if not provided all will be used)')
     parser.add_argument('-2', '--fields2', nargs='+', type=str, help='one or more column names from the second file that should be used (if not provided all will be used)')
     args = vars(parser.parse_args())
-    if not args['FILE2']:
+    if args['FILE1'] == '-' and args['FILE2'] == '-':
         parser.print_help()
         parser.exit(1)
     return args
@@ -31,6 +31,7 @@ def read(filename, fields):
     if not os.path.isfile(filename) and filename != '-': raise Exception(filename + ': no such file')
     file = sys.stdin if filename == '-' else io.open(filename, 'rb')
     text = file.read()
+    if text == '': raise Exception(filename + ': file is empty')
     text = text.decode(chardet.detect(text)['encoding'])
     data_io = io.StringIO(text) if sys.version_info >= (3, 0) else io.BytesIO(text.encode('utf8'))
     data = list(csv.reader(data_io))
@@ -45,14 +46,14 @@ def read(filename, fields):
     rows = {}
     for (i, row) in enumerate(reader):
         items = dict(row.items())
-        rows['%s|%s' % (filename, i)] = { key: items[key] for key in fields }
+        rows[i] = { key: items[key] for key in fields }
     return fields, dict(rows)
 
 def match(data1, data2):
     matches = []
-    for data1Key, data1Values in data1.items():
-        for data2Key, data2Values in data2.items():
-            if set(data1Values.values()) == set(data2Values.values()): matches.append((data1Key, data2Key))
+    for data1key, data1values in data1.items():
+        for data2key, data2values in data2.items():
+            if set(data1values.values()) == set(data2values.values()): matches.append((data1key, data2key))
     return matches
 
 def output(data1, data2, fields1, fields2, matches):
