95acf9d526a56de6eec4b71f6dab627ecc7b6149
Max Harlow <maxharlow@gmail.com>
2015-12-25 14:46:30 +0000

    Extracted processing, option to sort words

--- csvmatch.py
+++ csvmatch.py
@@ -15,8 +15,12 @@ def main():
         args = arguments()
         fields1, data1 = read(args['FILE1'], args['fields1'])
         fields2, data2 = read(args['FILE2'], args['fields2'])
-        data1processed = process_lowercase(data1) if args['ignore_case'] else data1
-        data2processed = process_lowercase(data2) if args['ignore_case'] else data2
+        processes = [
+            (process_lowercase, args['ignore_case']),
+            (process_sort, args['sort_words'])
+        ]
+        data1processed = processor(data1, processes)
+        data2processed = processor(data2, processes)
         matches = matcher(args['algorithm'])(data1processed, data2processed, fields1, fields2)
         results = output(data1, data2, fields1, fields2, matches)
         print(results)
@@ -29,6 +33,7 @@ def arguments():
     parser.add_argument('-1', '--fields1', nargs='+', type=str, help='one or more column names from the first file that should be used (if not provided all will be used)')
     parser.add_argument('-2', '--fields2', nargs='+', type=str, help='one or more column names from the second file that should be used (if not provided all will be used)')
     parser.add_argument('-i', '--ignore-case', action='store_true', help='perform case insensitive matching (by default it is case sensitive)')
+    parser.add_argument('-s', '--sort-words', action='store_true', help='sort words alphabetically before comparisons')
     parser.add_argument('-f', '--fuzzy', nargs='?', type=str, const='bilenko', dest='algorithm', help='whether to use a fuzzy match or not')
     args = vars(parser.parse_args())
     if args['FILE1'] == '-' and args['FILE2'] == '-':
@@ -36,8 +41,17 @@ def arguments():
         parser.exit(1)
     return args
 
+def processor(data, processes):
+    data_processed = data.copy()
+    for process, selected in processes:
+        data_processed = process(data_processed) if selected else data_processed
+    return data_processed
+
 def process_lowercase(data):
     return {key: {field: data[key][field].lower() for field in data[key]} for key in data}
+
+def process_sort(data):
+    return {key: {field: ' '.join(sorted(data[key][field].split(' '))) for field in data[key]} for key in data}
 
 def read(filename, fields):
     if not os.path.isfile(filename) and filename != '-': raise Exception(filename + ': no such file')
