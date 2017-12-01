6b3a16bf24ad54e03dbe360ba2cdb8126a8211f1
Max Harlow <maxharlow@gmail.com>
2017-12-01 13:54:16 +0000

    Better argument descriptions

--- cli.py
+++ cli.py
@@ -25,23 +25,23 @@ def main():
 
 def arguments():
     parser = argparse.ArgumentParser(description='find (fuzzy) matches between two CSV files')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
     parser.add_argument('FILE1', nargs='?', default='-', help='the first CSV file')
     parser.add_argument('FILE2', nargs='?', default='-', help='the second CSV file')
-    parser.add_argument('--enc1', type=str, help='encoding of the first file (autodetected otherwise)')
-    parser.add_argument('--enc2', type=str, help='encoding of the second file (autodetected otherwise)')
-    parser.add_argument('-1', '--fields1', nargs='+', type=str, help='one or more column names from the first CSV file that should be used (all columns will be used otherwise)')
-    parser.add_argument('-2', '--fields2', nargs='+', type=str, help='one or more column names from the second CSV file that should be used (all columns will be used otherwise)')
-    parser.add_argument('-i', '--ignore-case', action='store_true', help='perform case-insensitive matching (it is case-sensitive otherwise)')
-    parser.add_argument('-l', '--filter', help='filter out terms from a given newline-separated list of regular expressions before comparisons')
-    parser.add_argument('-t', '--filter-titles', action='store_true', help='filter out name titles (Mr, Ms, etc) before comparisons')
-    parser.add_argument('-a', '--ignore-nonalpha', action='store_true', help='ignore non-alphanumeric characters')
-    parser.add_argument('-n', '--as-latin', action='store_true', help='convert to latin alphabet before comparisions')
-    parser.add_argument('-s', '--sort-words', action='store_true', help='sort words alphabetically before comparisons')
-    parser.add_argument('-j', '--join', type=str, default='inner', help='the type of join to use: \'inner\', \'left-outer\', \'right-outer\', or \'full-outer\' (default is an inner join)')
-    parser.add_argument('-o', '--output', nargs='+', type=str, help='fields that should be outputted, prefixed by \'1.\' or \'2.\' depending on their source file (otherwise uses whatever fields were used for comparisions)')
-    parser.add_argument('-f', '--fuzzy', nargs='?', type=str, const='bilenko', dest='algorithm', help='use a fuzzy match, and an optional specified algorithm: \'bilenko\', \'levenshtein\', or \'metaphone\' (default is \'bilenko\')')
+    parser.add_argument('--enc1', type=str, help='encoding of the first file (default is to autodetect)')
+    parser.add_argument('--enc2', type=str, help='encoding of the second file (default is to autodetect)')
+    parser.add_argument('-1', '--fields1', nargs='+', type=str, help='one or more column names from the first CSV file that should be used (default is all columns)')
+    parser.add_argument('-2', '--fields2', nargs='+', type=str, help='one or more column names from the second CSV file that should be used (default is all columns)')
+    parser.add_argument('-i', '--ignore-case', action='store_true', help='ignore case when comparing (default is case-sensitive)')
+    parser.add_argument('-l', '--filter', help='filter out terms from a newline-separated file of regular expressions when comparing')
+    parser.add_argument('-t', '--filter-titles', action='store_true', help='filter out a predefined list of name titles (Mr, Ms, etc) when comparing')
+    parser.add_argument('-a', '--ignore-nonalpha', action='store_true', help='ignore non-alphanumeric characters when comparing')
+    parser.add_argument('-n', '--as-latin', action='store_true', help='convert to latin alphabet when comparing')
+    parser.add_argument('-s', '--sort-words', action='store_true', help='sort words alphabetically when comparing')
+    parser.add_argument('-j', '--join', type=str, default='inner', help='the type of join to use: \'inner\', \'left-outer\', \'right-outer\', or \'full-outer\' (default is inner)')
+    parser.add_argument('-o', '--output', nargs='+', type=str, help='space-separated list of fields that should be outputted, prefixed by \'1.\' or \'2.\' depending on their source file (default is the field lists if specified, otherwise all columns)')
+    parser.add_argument('-f', '--fuzzy', nargs='?', type=str, const='bilenko', dest='algorithm', help='perform a fuzzy match, and an optional specified algorithm: \'bilenko\', \'levenshtein\', or \'metaphone\' (default is bilenko)')
     parser.add_argument('-r', '--threshold', type=float, default=0.6, help='the threshold for a fuzzy match as a number between 0 and 1 (only supported by levenshtein) (default is 0.6)')
-    parser.add_argument('-v', '--version', action='version', version=__version__)
     args = vars(parser.parse_args())
     if args['FILE1'] == '-' and args['FILE2'] == '-':
         parser.print_help(sys.stderr)
@@ -71,7 +71,7 @@ def read(filename, encoding):
 def format(results, keys):
     lines = [[value if sys.version_info >= (3, 0) else value.encode('utf8') for value in row.values()] for row in results]
     writer_io = io.StringIO() if sys.version_info >= (3, 0) else io.BytesIO()
-    writer = csv.writer(writer_io, lineterminator='\n') # can't use dictwriter as we want to print headers even when there's no results
+    writer = csv.writer(writer_io, lineterminator='\n') # can't use dictwriter as headers are printed even when there's no results
     writer.writerow(keys)
     writer.writerows(lines)
     return writer_io.getvalue()
