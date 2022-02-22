f60cd94da9e5b677d01a312b0e1a1af27628fdcf
Max Harlow <maxharlow@gmail.com>
2022-02-22 18:30:45 +0000

    Bump version

--- cli.py
+++ cli.py
@@ -9,7 +9,7 @@ import chardet
 import tqdm
 import csvmatch
 
-__version__ = '1.21'
+__version__ = '1.22'
 
 def main():
     logging.captureWarnings(True)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='csvmatch',
-    version='1.21',
+    version='1.22',
     description='Find (fuzzy) matches between two CSV files in the terminal.',
     long_description=open('README.md').read(),
     author='Max Harlow',
